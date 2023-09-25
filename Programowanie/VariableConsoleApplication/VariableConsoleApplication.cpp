cffed497ddfed3d59e1ee161668c9aa7b5303753
Test <test@test.pl>
2023-09-25 16:00:11 +0200

    1

---
+++ Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -0,0 +1,17 @@
+// VariableConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+}
+
+/*
+Algorytm - skoñczony zbiór instrukcji, który rozwi¹zuje zadany problem , okreœla te¿ kolejnoœæ wykonywania instrukcji
+Zapis algorytmu:
+rysunki
+
+*/
