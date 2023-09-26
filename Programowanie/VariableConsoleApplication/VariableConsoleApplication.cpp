0acd55cae651504c2a166766b6c0a0e8a3717c69
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-09-26 09:46:36 +0200

    zmienne

--- Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
+++ Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -1,17 +1,54 @@
-// VariableConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int num_from_user;
 
+    std::cout << "Podaj liczbe: ";
+    std::cin >> num_from_user;
+    std::cout << "Podales: " << num_from_user;
+    
 }
 
 /*
 Algorytm - skoñczony zbiór instrukcji, który rozwi¹zuje zadany problem , okreœla te¿ kolejnoœæ wykonywania instrukcji
 Zapis algorytmu:
-rysunkias
+    -rysunki
+    -opis s³owny
+    -w punktach
+    -schemat blokowy
+    -kod Ÿród³owy danego jezyka
+    -pseudokod
+ 
+Zmienna - pewien obszar w pamiêci operacyjnej,
+w kórej mo¿na w danej chwili przechowaæ 1 dan¹
+
+Instrukcja deklaracji zmiennej:
+typ_zmiennej nazwa_zmiennej
+Typ zmiennej - wielkoœæ obszaru pamiêci, interpretacja ci¹gu bitów
+    int - 4 bajtowa liczba ca³kowita ze znakiem <-2 147 482 648, 2 147 483 647>
+    short - 2 bajtowa liczba ca³kowita ze znakiem <-32 768, 32 767->
+    long - to samo co int
+    long long - 8 bajtowa liczba ze znakiem 
+    unsigned - zmienna bez znaku <0, 2*max + 1>
+    float - 4 bajtowa liczba rzeczywista, dokladnosc 6-7 cyfr po przecinku
+    double - 8 bajtowa liczba rzeczywista, dokladnosc 15-16 cyfr po przecinku
+    long double - 12 bajtowa liczba rzeczywista, dokladnosc 19-20 cyfr po przecinku
+
+Nazwa zmiennnej - nazwa obszaru w pamiêci, identyfikator
+Warunki niezbêdne:
+    -dozwolone znaki: alfabet angielski aA-zZ, cyfry arabskie 0-9, podkreslenie (_)
+    -pierwszy znak nie zaczyna siê od cyfry
+    -unikalny
+    -nie mo¿e to byæ zarezerwowane z³owo kluczowe danego jezyka
+
+Warunki progrmistów:
+    -nazwa zmiennnej powinna oddawaæ charakter przechowywanych danych (wiek_osoby, wiekOsoby)
+    -piszemy po angielsku
+
+
+
+
+
 
 */
