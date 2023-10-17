3bc31532866ef0f36f919b10764d549a0768a2c6
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-10-17 09:35:44 +0200

    zadania z if

--- Programowanie/InstrucionIfConsoleApp/InstrucionIfConsoleApp.cpp
+++ Programowanie/InstrucionIfConsoleApp/InstrucionIfConsoleApp.cpp
@@ -34,12 +34,92 @@ void task2()
 	if (rest != 0)
 		std::cout << "Podana liczba jest nieparzysta!\n";
 	else
-		std::cout << "Podana liczba ejst parzysta!\n";
+		std::cout << "Podana liczba jest parzysta!\n";
+}
+
+//Napisz program ktory wyswietli informacje czy liczba jest z zakresu <1 , 10)
+void task3()
+{
+	int number;
+
+	std::cout << "Podaj liczbe: ";
+	std::cin >> number;
+
+	//wersja 1
+	if (number >= 1)
+	{
+		if (number < 10)
+		{
+			std::cout << "Liczba znajduje sie w zakresie\n";
+		}
+		else
+		{
+			std::cout << "Liczba nie znajduje sie w zakresie\n";
+		}
+	}
+	else
+		std::cout << "Liczba nie znajduje sie w zakresie\n";
+
+	//wersja 2
+	if (number < 10 && number >= 1)
+	{
+		std::cout << "Liczba znajduje sie w zakresie\n";
+	}
+	else
+	{
+		std::cout << "Liczba nie znajduje sie w zakresie\n";
+	}
+
+	//wersja 3
+	if (number < 1 || number >= 10)
+	{
+		std::cout << "Liczba znajduje sie w zakresie\n";
+	}
+	else
+	{
+		std::cout << "Liczba nie znajduje sie w zakresie\n";
+	}
+
+	//wersja 4
+	if (!(number < 10 && number >= 1))
+	{
+		std::cout << "Liczba nie znajduje sie w zakresie\n";
+	}
+	else
+	{
+		std::cout << "Liczba znajduje sie w zakresie\n";
+	}
+}
+
+//dzielenie 2 liczb
+void task4()
+{
+	int firstNumber, secondNumber, quo;
+	std::cout << "Podaj 1 liczbe: ";
+	std::cin >> firstNumber;
+	std::cout << "Podaj 2 liczbe: ";
+	std::cin >> secondNumber;
+
+	if (secondNumber != 0)
+	{
+		quo = firstNumber / secondNumber;
+		std::cout << "Wynuik = " << quo << "\n";
+	}
+	else
+	{
+		std::cout << "Nie dzielimy przez 0!\n";
+	}
+}
+
+//Napisz program, który poprosi u¿ytkownika o podanie dwóch liczb ca³kowitych i sprawdzi, czy s¹ one równe. Wyœwietl odpowiedni komunikat.
+void task5()
+{
+
 }
 
 int main()
 {
-	task2();
+	task4();
 }
 
 /*
@@ -50,4 +130,30 @@ Operatory warunkowe:
 	== - rowny
 	!= - rozny
 
+	&& - AND
+	|| - OR
+	! - NOT
+
+	a	b	a&&b	a||b	!a		a XOR b
+	F	F	F		 F		 T			F		
+	F	T	F		 T		 T			T
+	T	F	F		 T		 F			T
+	T	T	T		 T		 F			F
+
+
+*/
+
+/*
+*.
+*. Napisz program, który poprosi u¿ytkownika o podanie wieku i sprawdzi, czy osoba jest pe³noletnia. Wyœwietl odpowiedni komunikat.
+*. Napisz program, który poprosi u¿ytkownika o podanie liczby ca³kowitej i obliczy jej wartoœæ bezwzglêdn¹. Wyœwietl wynik.
+*. Napisz program, który poprosi u¿ytkownika o podanie liczby od 1 do 7 i wyœwietli odpowiadaj¹cy mu dzieñ tygodnia.
+*. Napisz program, który poprosi u¿ytkownika o podanie dwóch liczb ca³kowitych i wyœwietli wiêksz¹ z nich.
+*. Napisz program, który poprosi u¿ytkownika o podanie roku i sprawdzi, czy jest to rok przestêpny. Wyœwietl odpowiedni komunikat.
+*. Napisz program, który poprosi u¿ytkownika o podanie liczby ca³kowitej i sprawdzi, czy jest ona podzielna zarówno przez 3, jak i przez 5. Wyœwietl odpowiedni komunikat.
+*. Napisz program, który poprosi u¿ytkownika o podanie masy cia³a (w kilogramach) i wzrostu (w metrach). Na podstawie tych danych oblicz wskaŸnik BMI (Body Mass Index) i wyœwietl odpowiedni komunikat informuj¹cy o stanie zdrowia.
+*. Napisz program, który poprosi u¿ytkownika o podanie d³ugoœci trzech odcinków i sprawdzi, czy mo¿na zbudowaæ z nich trójk¹t. Wyœwietl odpowiedni komunikat.
+*. Napisz program, który poprosi u¿ytkownika o podanie liczby dodatniej i obliczy jej pierwiastek kwadratowy, jeœli jest to mo¿liwe. Wyœwietl wynik lub odpowiedni komunikat, jeœli liczba jest ujemna.
+*. Program sprawdzaj¹cy czy podana data jest poprawna (np. sprawdzaj¹c, czy dzieñ jest z zakresu od 1 do 31, miesi¹c od 1 do 12 itd.)
+*. 
 */
