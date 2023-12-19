4c59664185d3ddb6e14198552fb2f0852e7d744f
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-12-19 09:52:32 +0100

    zad8 zacz

--- Programowanie/CharactersConsoleApplication/CharactersConsoleApplication.cpp
+++ Programowanie/CharactersConsoleApplication/CharactersConsoleApplication.cpp
@@ -1,4 +1,19 @@
+//01000001 01110010 01110100 01110101 01110010 01001011
 #include <iostream>
+
+/*
+*
+*
+*
+* Program sprawdzaj¹cy czy podany ci¹g znaków jest palindromem (czyli takim, który czytany od ty³u jest taki sam, jak czytany od przodu, np. "kajak")
+* Program sprawdzaj¹cy czy podane dwa s³owa s¹ anagramami (czyli takimi, które zawieraj¹ te same litery, ale w innym uk³adzie, np. "klasa" i "salka")
+* Program wyci¹gaj¹cy informacje z numeru PESEL
+* Program implementuj¹cy algorytm szyfrowania Cezara (proste szyfrowanie, w którym ka¿dy znak w tekœcie jest zastêpowany innym znakiem, przesuniêtym o sta³¹ liczbê pozycji w alfabecie).
+
+* Program który na wejœciu przyjmie równanie a na wyjœciu da równanie w odwrotnej notacji polskiej ONP. Np. na wejœciu 2+3*4 na wyjœcu da 234*+
+* Program, który na wejœciu przyjmie rówanie w ONP a na wyjœciu wyœwietli wynik rówania.
+* Algorytm szyfrowania AtBash Cipher - algorytm szyfruj¹cy, w którym ka¿da litera jest zamieniana na literê z przeciwnej strony alfabetu (np. A na Z, B na Y itd.)
+*/
 
 //Napisz program ktory pobierze znak
 void task1()
@@ -19,9 +34,9 @@ void task2()
 
 	//if(characterFromUser >= 97 && characterFromUser <= 122)
 	if (characterFromUser >= 'a' && characterFromUser <= 'z')
-		std::cout << "Ma³a litera alfabetu\n.";
+		std::cout << "Ma³a litera alfabetu.\n";
 	else
-		std::cout << "Nie ma³a litera alfabetu\n.";
+		std::cout << "Nie ma³a litera alfabetu.\n";
 
 	/*char x = 'a';
 	std::cout << x;
@@ -29,19 +44,136 @@ void task2()
 	std::cout << x;
 
 	*/
+}
+
+//Napisz program, który poprosi o podanie imienia i sie przywita.
+void task3()
+{
+	std::string username = "";
+	std::cout << "Podaj imie: ";
+	std::cin >> username;
+	std::cout << "Witaj " << username << "\n";
+}
+
+//Napisz program, który sprawdzi czy podane has³o jest poprawne
+//np. jesli haslo = abc123 wyswietli poprawne has³o.
+//jesli nie wyswietli niepoprawne has³o.
+void task4()
+{
+	std::string password;
+	std::cout << "Podaj has³o: ";
+	std::cin >> password;
+
+	if (password == "abc123")
+		std::cout << "Haslo poprawne\n";
+	else
+		std::cout << "Haslo niepoprawne\n";
+}
+
+//Napisz program, który wczyta ³añcuch znaków i policzy ile jest ma³ych liter 'a'.
+void task5()
+{
+	std::string textFromUser;
+	std::cout << "Podaj ³añcuch znaków\n";
+	std::cin >> textFromUser;
+
+	int numerOfCharacters = 0;
+
+	/*std::cout << "Pierwszy znak to " << textFromUser[0] << "\n";
+	textFromUser[1] = 'x';
+	std::cout << "Iloœæ znakow " << textFromUser.length() << "\n";*/
+
+	for (int i = 0; i < textFromUser.length(); i++)
+	{
+		if (textFromUser[i] == 'a')
+			numerOfCharacters++;
+	}
+	std::cout << "Jest " << numerOfCharacters << " liter 'a'\n";
+}
+
+//Napisz program, który bêdzie prosi³ o has³o. Nie przepuœci dalej dopóki nie zostanie ono podane prawid³owo.
+void task6()
+{
+	std::string password;
+	std::cout << "Podaj has³o: ";
+	std::cin >> password;
+
+	do
+	{
+		std::cout << "Podaj has³o: ";
+		std::cin >> password;
+		if (password == "abc123")
+			std::cout << "Haslo poprawne\n";
+		else
+			std::cout << "Haslo niepoprawne\n";
+
+	} while (password != "abc123");
 
 }
+
+//01000001 01110010 01110100 01110101 01110010 01001011
+//Napisz program, który pobiera od u¿ytkownika ci¹g znaków i wyœwietla liczbê samog³osek i spó³g³osek w tym ci¹gu.
+void task7()
+{
+	std::string characterFromUser;
+	std::cout << "Podaj cig znaków: ";
+	std::cin >> characterFromUser;
+
+	//samog³oska
+	int numberOfVowel = 0;
+	//spó³g³oska
+	int numberOfConsonant = 0;
+
+	for (int i = 0; i < characterFromUser.length(); i++)
+	{
+		if (characterFromUser[i] == 'a'
+			|| characterFromUser[i] == '¹'
+			|| characterFromUser[i] == 'e'
+			|| characterFromUser[i] == 'ê'
+			|| characterFromUser[i] == 'i'
+			|| characterFromUser[i] == 'o'
+			|| characterFromUser[i] == 'u'
+			|| characterFromUser[i] == 'y')
+			numberOfVowel++;
+		else if (characterFromUser[i] >= 'a' && characterFromUser[i] <= 'z')
+			numberOfConsonant++;
+	}
+	std::cout << "W tym ciagu jest " << numberOfVowel << " samog³osek i " << numberOfConsonant << " spó³g³osek\n";
+}
+
+//Poproœ u¿ytkownika o wprowadzenie liczby ca³kowitej w systemie dziesiêtnym.
+//Nastêpnie skonwertuj tê liczbê na system dwójkowy (binarny) i wyœwietl wynik.
+void task8()
+{
+	int numFromUser;
+	int tmpNum;
+	std::string tmpNumInBin;
+	std::string numberInBin;
+	std::cout << "Podaje liczbe: ";
+	std::cin >> numFromUser;
+	tmpNum = numFromUser;
+
+	do
+	{
+		tmpNumInBin = tmpNum % 2;
+		numberInBin = numberInBin + tmpNumInBin;
+		tmpNum / 2;
+	} while (tmpNum == 0);
+
+	std::cout << numberInBin << "\n";
+}
+
 
 int main()
 {
 	setlocale(LC_CTYPE, "polish");
 
-	task2();
+	task8();
 }
 
 /*
 Typy znakow:
-* char - 1 bajt ca³owito liczbowa <-128; 127>
-
+* char - 1 bajt ca³owito liczbowa <-128; 127> 'a'
+* std::string - ciag znakow "abc...123"
 
 */
