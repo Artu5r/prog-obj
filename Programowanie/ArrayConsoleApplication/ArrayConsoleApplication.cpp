7a53e9ab736eb7a074c00fddd7db164e85f4fc8a
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-01-18 08:12:15 +0100

    zad4 v2

--- Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
+++ Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
@@ -37,7 +37,7 @@ void task2()
 		std::cout << numbers[i] << ", ";
 	}
 	std::cout << "\n";
-	
+
 	int sum = 0;
 	for (int i = 0; i < ARRAY_SIZE; i++)
 	{
@@ -47,9 +47,89 @@ void task2()
 	std::cout << "Œrednia wynosi: " << avg << "\n";
 }
 
+//Napisz program, który uzupe³ni tablicê liczbami losowymi a nastêpnie znajdzie minimum oraz maksimum.
+void task3()
+{
+	const short LOWER_RANGE = 0;
+	const short UPPER_RANGE = 70;
+
+	const unsigned short ARRAY_SIZE = 100;
+	int numbers[ARRAY_SIZE];
+
+	srand(time(0));
+
+	std::cout << "wylosowane liczby:\n";
+	for (int i = 0; i < ARRAY_SIZE; i++)
+	{
+		numbers[i] = rand() % (UPPER_RANGE - LOWER_RANGE + 1) + LOWER_RANGE;
+		std::cout << numbers[i] << ", ";
+	}
+	std::cout << "\n";
+
+	int max = numbers[0];
+	for (int i = 1; i < ARRAY_SIZE; i++)
+	{
+		if (numbers[i] > max)
+			max = numbers[i];
+	}
+
+	std::cout << "Max to: " << max << "\n";
+
+	int min = numbers[0];
+	for (int i = 1; i < ARRAY_SIZE; i++)
+	{
+		if (numbers[i] < min)
+			min = numbers[i];
+	}
+
+	std::cout << "Min to: " << min << "\n";
+}
+
+//Napisz program, który wyznaczy wszystkie liczby pierwsze od 2 do zadeklarowanego zakresu. Metoda sito Eratostenesa.
+void task4()
+{
+	const long long UPPER_RANGE = 70;
+
+	//wersja 1
+
+	for (long long numberToCheck = 2; numberToCheck <= UPPER_RANGE; numberToCheck++)
+	{
+		bool isPrime = true;
+		for (long long i = 2; i <= numberToCheck / 2; i++)
+		{
+			if (numberToCheck % i == 0)
+			{
+				isPrime = false;
+				break;
+			}
+		}
+
+		if (isPrime /*== true*/)
+			std::cout << numberToCheck << ", ";
+	}
+	std::cout << "Gotowe\n";
+
+	//wersja 2
+	long long sieveToErastosthenes[UPPER_RANGE + 1];
+
+	for (long long  i = 0; i < length; i++)
+	{
+
+	}
+
+	for (int number = 2; number <= UPPER_RANGE; number++)
+	{
+		if (number  nie wykreslony)
+		{
+			for(int numberToCrossOut = number + number; numberToCrossOut <= UPPER_RANGE; numberToCrossOut++)
+
+		}
+	}
+}
+
 int main()
 {
 	setlocale(LC_CTYPE, "polish");
 
-	task2();
+	task4();
 }
