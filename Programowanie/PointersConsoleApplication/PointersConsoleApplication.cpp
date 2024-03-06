a90285b1962d9a28f5e7447955f45da7a283bc32
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-03-06 09:53:30 +0100

    wskazniki

---
+++ Programowanie/PointersConsoleApplication/PointersConsoleApplication.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+void task1()
+{
+	int firstNumber = 20;
+	int secondNumber = 43;
+	int tab[2] = { 15, 52 };
+
+	std::cout << "Wartosc pierwszej zmiennej " << firstNumber << "\n";
+	std::cout << "Adres pierwszej zmiennej " << &firstNumber << "\n";
+	std::cout << "Wartosc drugiej zmiennej " << secondNumber << "\n";
+	std::cout << "Adres drugiej zmiennej " << &secondNumber << "\n";
+	*(&secondNumber) = 47;
+	std::cout << "Wartosc drugiej zmiennej " << secondNumber << "\n";
+
+	std::cout << "Zawartosc zerowej komorki " << tab[0] << "\n";
+	std::cout << "Zawartosc pierwszej komorki " << tab[1] << "\n";
+	std::cout << "Adres zerowej komorki " << &tab[0] << "\n";
+	std::cout << "Adres pierwszej komorki " << &tab[1] << "\n";
+	std::cout << "Adres tablicy  " << tab << "\n";
+	std::cout << "Adres tablicy  " << &tab << "\n";
+}
+
+void task2()
+{
+
+}
+
+int main()
+{
+
+}
