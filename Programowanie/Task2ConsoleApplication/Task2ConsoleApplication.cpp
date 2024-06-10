2293408d7eb7d2d96a6c836aaee2142e05f911a3
awadad <adadada@gmail.com>
2024-06-10 15:28:40 +0200

    z steper

--- Programowanie/Task2ConsoleApplication/Task2ConsoleApplication.cpp
+++ Programowanie/Task2ConsoleApplication/Task2ConsoleApplication.cpp
@@ -10,29 +10,156 @@ void setCursor(int x, int y)
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
 
-int main()
-{	
-	int boardCoordinatesLeftTop = 20;
-	int boardLeftTopX = 20;
-	int boardLeftTopY = 20;
-	int boardRightBottomX = 80;
-	int boardRightBottomY = 40;
+enum keyCode
+{
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT,
+	ESC
+};
+
+struct point
+{
+	unsigned short x;
+	unsigned short y;
+};
+
+struct boardCoordinates
+{
+	point leftTop, rightBottom;
+};
+
+void getConsolResolution(int& consoleWidth, int& consoleHeight)
+{
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+
+	consoleWidth = csbi.srWindow.Right - csbi.srWindow.Left;
+	consoleHeight = csbi.srWindow.Bottom - csbi.srWindow.Top;
+}
 
 
-	setCursor(boardCoordinatesLeftTop, boardLeftTopY);
-	for (int i = boardLeftTopX; i <= boardRightBottomX; i++)
-		std::cout << "#";
+keyCode codeChar(unsigned char charToCode)
+{
+	if (charToCode == 'w' || charToCode == 72)
+		return keyCode::UP;
+	if (charToCode == 's' || charToCode == 80)
+		return keyCode::DOWN;
+	if (charToCode == 'a' || charToCode == 75)
+		return keyCode::LEFT;
+	if (charToCode == 'd' || charToCode == 77)
+		return keyCode::RIGHT;
+	if (charToCode == 27)
+		return keyCode::ESC;
+}
 
-	for (int row = boardLeftTopY + 1; row < boardRightBottomY; row++)
+keyCode getKeyCode(keyCode prevKeyCode)
+{
+	unsigned char currentChar = 0;
+	keyCode currentKeyCode = prevKeyCode;
+	if (_kbhit())
 	{
-		setCursor(boardLeftTopX, row);
-		std::cout << "#";
+		currentChar = _getch();
+		if (currentChar == 224 || currentChar == 0)
+		{
+			currentChar = _getch();
+			if (currentChar == 72
+				|| currentChar == 80
+				|| currentChar == 75
+				|| currentChar == 77
+				|| currentChar == 27)
+				currentKeyCode = codeChar(currentChar);
+		}
+		else if (currentChar == 'w'
+			|| currentChar == 's'
+			|| currentChar == 'a'
+			|| currentChar == 'd'
+			|| currentChar == 27)
+			currentKeyCode = codeChar(currentChar);
+	}
+	return currentKeyCode;
+}
 
-		setCursor(boardRightBottomX, row);
-		std::cout << "#";
+void changeCoordinate(point& coordinate, keyCode currentKeyCode, int consoleWidth, int consoleHeight)
+{
+	switch (currentKeyCode)
+	{
+	case keyCode::UP:
+		if (coordinate.y > 0)
+			coordinate.y--;
+		else
+			coordinate.y = consoleHeight;
+		break;
+	case keyCode::DOWN:
+		if (coordinate.y < consoleHeight)
+			coordinate.y++;
+		else
+			coordinate.y = 0;
+		break;
+	case keyCode::LEFT:
+		if (coordinate.x > 0)
+			coordinate.x--;
+		else
+			coordinate.x = consoleWidth;
+		break;
+	case keyCode::RIGHT:
+		if (coordinate.x < consoleWidth)
+			coordinate.x++;
+		else
+			coordinate.x = 0;
+		break;
+	default:
+		break;
+	}
+}
+
+int main()
+{
+	boardCoordinates board;
+	point boardCoordinates;
+	board.leftTop.x = 20;
+	board.leftTop.y = 20;
+	board.rightBottom.x = 80;
+	board.rightBottom.y = 40;
+
+	boardCoordinates.x = 20;
+	boardCoordinates.y = 20;
+
+
+	int consoleHeight, consoleWidth;
+	getConsolResolution(consoleWidth, consoleHeight);
+	keyCode currentKeyCode = keyCode::RIGHT;
+
+	setCursor(board.leftTop.x, board.leftTop.y);
+
+
+	while (true)
+	{
+		changeCoordinate(boardCoordinates, currentKeyCode, consoleWidth, consoleHeight);
 	}
 
-	setCursor(boardLeftTopX, boardRightBottomY);
-	for (int i = boardLeftTopX; i <= boardRightBottomX; i++)
-		std::cout << "#";
+
+
+
+
+
+
+	//for (int i = board.leftTop.x; i <= board.rightBottom.x; i++)
+	//	std::cout << "#";
+
+	//for (int row = board.leftTop.y + 1; row < board.rightBottom.y; row++)
+	//{
+	//	setCursor(board.leftTop.x, row);
+	//	std::cout << "#";
+
+	//	setCursor(board.rightBottom.x, row);
+	//	std::cout << "#";
+	//}
+
+	//setCursor(board.leftTop.x, board.rightBottom.y);
+	//for (int i = board.leftTop.x; i <= board.rightBottom.x; i++)
+	//	std::cout << "#";
+
+
 }
---
+++ Programowanie/Task3ConsoleApplication/Task3ConsoleApplication.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <conio.h>
+
+void setCursor(int x, int y)
+{
+	COORD c;
+	c.X = x;
+	c.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+
+void drawSecounds(int secounds)
+{
+	int x = 0;
+	int y = 0;
+	setCursor(x, y);
+
+	
+	std::cout << secounds;
+}
+
+void drawMinutes(int minutes)
+{
+	int x = 3;
+	int y = 0;
+	setCursor(x, y);
+
+	
+	std::cout << minutes;
+}
+
+void drawHours(int hours)
+{
+	int x = 6;
+	int y = 0;
+	setCursor(x, y);
+
+	
+	std::cout << hours;
+}
+
+int main()
+{
+	int secounds = 55;
+	int minutes = 0;
+	int hours = 0;
+	
+	bool quit = false;
+
+	unsigned char currentChar = 0;
+
+	while (!quit)
+	{
+		if (_kbhit())
+		{
+			currentChar = _getch();
+			if (currentChar == 13)
+			{
+				while (!quit)
+				{
+					if (_kbhit())
+					{
+						currentChar = 0;
+						currentChar = _getch();
+						if (currentChar == 13)
+							quit = true;
+					}
+
+					Sleep(1000);
+					secounds++;
+
+					drawSecounds(secounds);
+					if (secounds >= 60)
+					{
+						minutes++;
+						secounds = 0;
+					}
+						
+					drawMinutes(minutes);
+					
+					if (minutes >= 60)
+					{
+						hours++;
+						minutes = 0;
+					}	
+
+					drawHours(hours);
+
+
+				}
+			}
+
+		}
+	}
+
+
+
+}
