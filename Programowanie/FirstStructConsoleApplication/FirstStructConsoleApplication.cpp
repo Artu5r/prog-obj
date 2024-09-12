352bf3a0efc489c93ab589ea44c1634fdebe0560
a <a@a.pl>
2024-09-12 09:51:16 +0200

    imp header

---
+++ Programowanie/FirstStructConsoleApplication/FirstStructConsoleApplication.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Task1.cpp"
+
+int main()
+{
+	task1();
+}
---
+++ Programowanie/FirstStructConsoleApplication/Task1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+//  Napisz program który obliczy odlegloœæ punktu 2D od œrodka uk³adu wspó³rzêdnych.
+void task1()
+{
+	double x, y;
+	std::cout << "Podaj x: ";
+	std::cin >> x;
+	std::cout << "\nPodaj y: ";
+	std::cin >> y;
+
+	double distance = sqrt(x * x + y * y);
+	std::cout << "\nOdleglosc = " << distance;
+}
