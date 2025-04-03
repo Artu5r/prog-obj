06da3162d6ab255632d807183c9d6ff768d358a4
q <q>
2025-04-03 12:30:02 +0200

    zad4.2 niskoncz

--- Programowanie/MaturaSprawdzianConsoleApplication/MaturaSprawdzianConsoleApplication.cpp
+++ Programowanie/MaturaSprawdzianConsoleApplication/MaturaSprawdzianConsoleApplication.cpp
@@ -14,7 +14,7 @@ int reversedLiczba(int n)
 	return rev;
 }
 
-int main()
+void zad4()
 {
 	ifstream file("liczby.txt");
 	int numbers[100];
@@ -22,11 +22,74 @@ int main()
 	{
 		file >> numbers[i];
 	}
-
-	int numeryodw4_0[100];
+	cout << "zad4\n";
+	int revNumbers[100];
 	for (int i = 0; i < 100; i++)
 	{
-		numeryodw4_0[i] = reversedLiczba(numbers[i]);
-		cout << numeryodw4_0[i] << endl;
+		revNumbers[i] = reversedLiczba(numbers[i]);
+		cout << revNumbers[i] << endl;
 	}
+}
+
+void zad4_1()
+{
+	ifstream file("liczby.txt");
+	int numbers[100];
+	for (int i = 0; i < 100; i++)
+	{
+		file >> numbers[i];
+	}
+	cout << "zad4.1\n";
+	int revNumbers[100];
+	for (int i = 0; i < 100; i++)
+	{
+		revNumbers[i] = reversedLiczba(numbers[i]);
+	}
+
+	for (int i = 0; i < 100; i++)
+	{
+		if (revNumbers[i] % 17 == 0)
+		{
+			cout << revNumbers[i] << endl;
+		}
+	}
+}
+
+void zad4_2()
+{
+	ifstream file("przyklad.txt");
+	int numbers[100];
+	for (int i = 0; i < 100; i++)
+	{
+		file >> numbers[i];
+	}
+	cout << "zad4.2\n";
+	int revNumbers[100];
+	for (int i = 0; i < 100; i++)
+	{
+		revNumbers[i] = reversedLiczba(numbers[i]);
+	}
+	
+	int absoluteValue[100];
+	int tmp;
+
+	for (int i = 0; i < 100; i++)
+	{
+		if (numbers[i] < 0)
+		{
+			absoluteValue[i] = numbers[i] * -1;
+		}
+		else
+		{
+			absoluteValue[i] = numbers[i];
+		}
+
+		tmp = revNumbers[i] - absoluteValue[i];
+	}
+	
+}
+
+int main()
+{
+	zad4_2();
 }
