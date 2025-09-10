7862141d040eae62c6663e1c0fc8df9411ea013a
Sala213 <Sala213@ezse.pl>
2025-09-10 15:52:51 +0200

    a

--- Programowanie/FirstConsoleApp/Program.cs
+++ Programowanie/FirstConsoleApp/Program.cs
@@ -4,3 +4,30 @@ Console.WriteLine("Hello, World!");
 string name = "Jan";
 string surname = "Kowalski";
 Console.WriteLine("Witaj " + name + " " + surname + " " + "tutaj!");
+Console.WriteLine("Witaj {0} {1} tutaj!", name, surname);
+Console.WriteLine($"Witaj {name} {surname} tutaj!");
+
+Console.WriteLine("Predkosc 40km\\h");
+Console.WriteLine(@"Predkosc 40km\h");
+
+int firstNumber = 5;
+int secoundNumber = firstNumber;
+secoundNumber++;
+Console.WriteLine($"Pierwsza liczba to {firstNumber}");
+Console.WriteLine($"Druga liczba to {secoundNumber}");
+
+//-------------------------------------------------------------------
+
+void ParametrV1(int p)
+{
+    Console.WriteLine($"Parametr w ParametrV1 {p}");
+    p++;
+    Console.WriteLine($"Parametr w ParametrV1 {p}");
+}
+
+firstNumber = 15;
+Console.WriteLine($"firstNumber przed {firstNumber}");
+ParametrV1(firstNumber);
+Console.WriteLine($"firstnumber po {firstNumber}");
+//ParametrV1(99);
+
