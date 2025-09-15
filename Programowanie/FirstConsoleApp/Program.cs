1fa4276518323fd67587ecee574f9dbaa67f30df
Sala213 <Sala213@ezse.pl>
2025-09-15 12:42:40 +0200

    a

--- Programowanie/FirstConsoleApp/Program.cs
+++ Programowanie/FirstConsoleApp/Program.cs
@@ -28,6 +28,32 @@ void ParametrV1(int p)
 firstNumber = 15;
 Console.WriteLine($"firstNumber przed {firstNumber}");
 ParametrV1(firstNumber);
-Console.WriteLine($"firstnumber po {firstNumber}");
+Console.WriteLine($"firstNumber po {firstNumber}");
 //ParametrV1(99);
+
+void ParametrV2(ref int p)
+{
+    Console.WriteLine($"Parametr w ParametrV2 {p}");
+    p = 19;
+    Console.WriteLine($"Parametr w ParametrV2 {p}");
+}
+
+firstNumber = 15;
+Console.WriteLine($"firstNumber przed {firstNumber}");
+ParametrV2(ref firstNumber);
+Console.WriteLine($"firstNumber po {firstNumber}");
+//ParametrV2(ref 99); //BLAD
+
+
+void ParametrV3(out int p)
+{
+    //Console.WriteLine($"Parametr w ParametrV3 {p}");
+    p++;
+    Console.WriteLine($"Parametr w ParametrV3 {p}");
+}
+
+int thirdNumber;
+ParametrV3(out thirdNumber);
+Console.WriteLine($"thirdNumber po {thirdNumber}");
+//ParametrV3(ref 99); //BLAD
 
