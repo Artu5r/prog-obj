90779a99c80817cd6d5906d4634fd3aaa1e43892
Artu5r <144451275+Artu5r@users.noreply.github.com>
2025-11-19 09:18:36 +0100

    Update Task1.cs

--- Programowanie/PracticalTasksConsoleApp/2025-01/Task1.cs
+++ Programowanie/PracticalTasksConsoleApp/2025-01/Task1.cs
@@ -1,89 +1,115 @@
-﻿using System;
+using PracticalTasksConsoleApp._2025_01;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+/*************************************
+ Nazwa: Komunikat
+ Opis: Wyświetla w konsoli przekazywany komunikat tekstowy
+ parametry: tresc
+ zwracany typ i opis: brak
+*********************************/
+
+/********************************
+ Nazwa: UstawProgram
+ Opis: Ustawia numer programu prania od (1-12)
+ parametry: numer 
+ zwracany typ i opis: int 
+**********************************/
+
+
+/*************************************
+Nazwa: On
+Opis: Włącza odkurzacz, wyświetla odpowiedni komunikat
+parametry: brak
+zwracany typ i opis: brak
+**********************************/
+
+/***********************************
+Nazwa: Off
+Opis: Wyłącza odkurzacz oraz wyświetla odpowiedni komunikat
+parametry: brak
+zwracany typ i opis: brak
+*************************************/
+
 namespace PracticalTasksConsoleApp._2025_01
 {
+
     internal class Task5
     {
         public void Task()
         {
-            WashingMachine washingMachine = new WashingMachine();
-            VacuumCleaner vacuumCleaner = new VacuumCleaner();
+            WashingMachine w1 = new WashingMachine();
+            Vacuum v1 = new Vacuum();
+            Console.Write("Podaj numer prania 1..12: ");
+            int number;
+            bool correct = int.TryParse(Console.ReadLine(), out number);
+            if (!correct) number = 0;
+            pralka.UstawProgram(numer);
+            Vacuum.On();
+            Vacuum.On();
+            Vacuum.On();
+            Vacuum.Komunikat("Odkurzacz wyładował się");
+            Vacuum.Off();
+        }
+    }
 
-            Console.WriteLine("Podaj numer prania 1..12");
-            string input = Console.ReadLine();
-            int enteredNumber;
 
-            if (int.TryParse(input, out enteredNumber))
+
+    class MachineDevice
+    {
+        public void Announcement(string tresc)
+        {
+            Console.WriteLine(tresc);
+        }
+    }
+
+    class WashingMachine : MachineDevice
+    {
+        private int programNumber = 0;
+
+        public int setProgram(int number)
+        {
+            if (number >= 1 && number <= 12)
             {
-                washingMachine.SetProgram(enteredNumber);
+                programNumber = number;
+                Announcement("Program został ustawiony");
             }
             else
             {
-                washingMachine.SetProgram(-1);
+                Announcement("Podano niepoprawny numer programu");
+                programNumber = 0;
             }
 
-            vacuumCleaner.TurnOn();
-            vacuumCleaner.TurnOn();
-            vacuumCleaner.TurnOn();
-
-            vacuumCleaner.DisplayMessage("Odkurzacz wyładował się");
-
-            vacuumCleaner.TurnOff();
-        }
-    }
-}
-
-public class Device
-{
-    public void DisplayMessage(string message)
-    {
-        Console.WriteLine(message);
-    }
-}
-
-public class WashingMachine : Device
-{
-    private int programNumber = 0;
-
-    public int SetProgram(int number)
-    {
-        if (number >= 1 && number <= 12)
-        {
-            this.programNumber = number;
-            DisplayMessage("Program został ustawiony");
-        }
-        else
-        {
-            this.programNumber = 0;
-            DisplayMessage("Podano niepoprawny numer programu");
-        }
-        return this.programNumber;
-    }
-}
-
-public class VacuumCleaner : Device
-{
-    private bool isOn = false;
-
-    public void TurnOn()
-    {
-        if (!this.isOn)
-        {
-            this.isOn = true;
-            DisplayMessage("Odkurzacz włączono");
+            return programNumber;
         }
     }
 
-    public void TurnOff()
+    class Vacuum : MachineDevice
     {
-        if (this.isOn)
+        private bool condition = false;
+
+        public void On()
         {
-            this.isOn = false;
-            DisplayMessage("Odkurzacz wyłączono");
+            if (!condition)
+            {
+                stan = true;
+                Announcement("Odkurzacz włączono");
+            }
+        }
+
+
+
+        public void Off()
+        {
+            if (condition)
+            {
+                stan = false;
+                Announcement("Odkurzacz wyłączono");
+            }
         }
     }
 }
