28c46bb0c144b7ef2793efe316e1dca91e069b29
Uczen <Uczen@LAPTOP-26CG11IL>
2026-09-09 10:59:51 +0200

    calculator, eventBlinding

---
+++ Programowanie/EventBlindingMauiApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EventBlindingMauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Programowanie/EventBlindingMauiApp/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace EventBlindingMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            if (rotationLabel is not null
+                && sender is Slider slider)
+            {
+                rotationLabel.Rotation = slider.Value;
+
+                labelValue.Text = slider.Value.ToString();
+            }
+        }
+    }
+}
