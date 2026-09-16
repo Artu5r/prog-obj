a5d2da8e5ab5b5f951179004775df8ca06dcad38
Uczen <Uczen@LAPTOP-26CG11IL>
2026-09-16 08:58:12 +0200

    ok

--- Programowanie/SimpleBindingCalculatorMauiApp/MainPage.xaml.cs
+++ Programowanie/SimpleBindingCalculatorMauiApp/MainPage.xaml.cs
@@ -8,7 +8,7 @@
         }
 
         public string firstNumber { get; set; }
-        public string secoundNumber { get; set; } 
+        public string secoundNumber { get; set; }
         private string returnMessage;
         public string ReturnMessage
         {
@@ -22,18 +22,22 @@
 
         private void Button_Clicked_1(object sender, EventArgs e)
         {
-            double firstN = Convert.ToDouble(firstNumber);
-            double secoundN = Convert.ToDouble(secoundNumber);
-            double suma = firstN + secoundN;
-            double ruznica = firstN - secoundN;
-            double iloczyn = firstN * secoundN;
-            double iloraz = firstN / secoundN;
+            if (firstNumber is not null && secoundNumber is not )
+            {
+                double firstN = Convert.ToDouble(firstNumber);
+                double secoundN = Convert.ToDouble(secoundNumber);
+                double suma = firstN + secoundN;
+                double ruznica = firstN - secoundN;
+                double iloczyn = firstN * secoundN;
+                double iloraz = firstN / secoundN;
 
-            string r = "Suma " + suma.ToString() + "\n";
-            r += "Ruznica " + ruznica.ToString() + "\n";
-            r += "Iloczyn " + iloczyn.ToString() + "\n";
-            r += "Iloraz " + iloraz.ToString() + "\n";
-            ReturnMessage = r;
+                string r = "Suma " + suma.ToString() + "\n";
+                r += "Ruznica " + ruznica.ToString() + "\n";
+                r += "Iloczyn " + iloczyn.ToString() + "\n";
+                r += "Iloraz " + iloraz.ToString() + "\n";
+                ReturnMessage = r;
+            }
+
         }
     }
 }
