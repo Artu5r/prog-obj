40fad1a00dcee55bdd9ee1c01764756bf365aa66
Uczen <Uczen@LAPTOP-26CG11IL>
2026-09-16 08:42:57 +0200

    calc

---
+++ Programowanie/SimpleBindingCalculatorMauiApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace SimpleBindingCalculatorMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public string firstNumber { get; set; }
+        public string secoundNumber { get; set; } 
+        private string returnMessage;
+        public string ReturnMessage
+        {
+            get { return returnMessage; }
+            set
+            {
+                returnMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            double firstN = Convert.ToDouble(firstNumber);
+            double secoundN = Convert.ToDouble(secoundNumber);
+            int suma = int.Parse(firstNumber) + int.Parse(secoundNumber);
+            ReturnMessage = "Sma"suma.ToString();
+        }
+    }
+}
---
+++ Programowanie/SimpleBindingCalculatorMauiApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SimpleBindingCalculatorMauiApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
